8a3d59e6de4ec8aa7ebebb1affee3eab5a2331f5
Dio-ni <200103264@stu.sdu.edu.kz>
2024-05-19 21:01:23 +0600

    Initial commit

---
+++ src/main/java/com/example/YoutubeToSpotifyPlaylist/YoutubeToSpotifyPlaylistApplication.java
@@ -0,0 +1,13 @@
+package com.example.YoutubeToSpotifyPlaylist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YoutubeToSpotifyPlaylistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YoutubeToSpotifyPlaylistApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/YoutubeToSpotifyPlaylist/YoutubeToSpotifyPlaylistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.YoutubeToSpotifyPlaylist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YoutubeToSpotifyPlaylistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
